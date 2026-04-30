26ec5ec89dd8137568915b896063bf9c75d56a41
MWillian <matheuswno02@gmail.com>
2026-04-30 20:01:14 -0300

    Setup inicial do projeto para utilizar frameworl EJS.
    - Instalação das dependencias do EJS
    - Criação de middleware de erro para views não encontradas
    - Criação de arquivo launch.json de execução de projeto
    -  Ajuste no entrypoint app.js para sobrescrita dos métodos dos formulários
    - Ajuste na ordem dos endpoints das rotas
    - Criação de arquivos iniciais das views.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join }  from 'path';
 import { router } from '../src/routes/index.js';
-import { middlewareDeErros } from '../src/middlewares/erros.middlewares.js';
+import methodOverride from 'method-override';
+import { naoEncontrado, middlewareDeErros } from '../src/middlewares/erros.middlewares.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views')); 
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use('/api', router);
+
+app.use(naoEncontrado);     
 app.use(middlewareDeErros);
 
 export default app;
